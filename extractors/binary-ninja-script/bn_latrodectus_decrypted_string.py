e28201c0e30f9be18ec782015a48ce88bf23c561
Icaro-Cesar <icarocesar630@gmail.com>
2024-05-09 10:01:02 -0300

    Latrodectus String Extractors (Python-only and BinaryNinja script)

---
+++ extractors/binary-ninja-script/bn_latrodectus_decrypted_string.py
@@ -0,0 +1,43 @@
+
+#   Title:  Binary Ninja Latrodectus String Decryptor Script
+#   Author: 0x0d4y
+
+def format_string(decrypted_data) -> str:
+	wide = decrypted_data[1] == 0
+	formated_string = ""
+
+	if not wide:
+		formated_string = decrypted_data.decode("utf8")
+	else:
+		formated_string = decrypted_data.decode("utf-16le")
+		
+	if formated_string.isascii():
+		return formated_string
+	
+	return ""
+
+
+def decrypt_string(data_enc: bytes, xor_key: int) -> str:
+    decrypted_string = bytearray()
+    for enc_data in data_enc:
+        xor_key += 1
+        print("XOR key +1:", hex(xor_key))
+        decrypted_string.append(enc_data ^ (xor_key & 0xFF))
+    return format_string(decrypted_string)
+
+decrypt_function_address = # <= put here the address of the decrypt function
+cross_references =  bv.get_code_refs(decrypt_function_address)
+dec_strings = []
+
+for xrefs in cross_references:
+    encrypted_data = xrefs.mlil.params[0].constant   # Take the address of the encrypted data block
+    encrypted_data_block = bv.read(encrypted_data, 6)
+    decryption_key = encrypted_data_block[0]   # The XOR key is the first byte of the encrypted data block
+    print("\nInitial Byte Key:", hex(decryption_key))
+    data_lenght_calc = encrypted_data_block[0] ^ encrypted_data_block[4]    # data lenght calculation => The 1st byte of the data block ^ 5th byte of the data block
+    encrypt_full_data_lenght = bv.read(encrypted_data, data_lenght_calc + 6)
+    encrypted_data_only = encrypt_full_data_lenght[6:6+data_lenght_calc]    # Encrypted data block only, sixth byte and so on
+    print("Encrypted Data Block:", encrypted_data_only)
+    decrypted_str = decrypt_string(encrypted_data_only, decryption_key)
+    #bv.set_comment_at(xrefs.address, decrypted_str)          # Set comments on each Cross-Reference of the XOR Decryption function
+    print("Decrypted String:", decrypted_str)
---
+++ extractors/python-only/latrodectus_decrypted_string.py
@@ -0,0 +1,53 @@
+#
+#   Title:  Latrodectus String Decryptor Script
+#   Author: 0x0d4y
+#
+
+import pefile
+
+def format_string(encoded_string: bytes) -> str:
+    try:
+        formated_string = encoded_string.decode('utf-8')
+        if formated_string.isascii():
+            return formated_string
+    except UnicodeDecodeError:
+        pass
+
+    return "Not an ASCII String"
+
+# Latrodectus String Decryption Algorithm
+def decrypt_string(data_enc: bytes, xor_key: int) -> str:
+    decrypted_strings = bytearray()
+    for enc_data in data_enc:
+        xor_key += 1
+	print(hex("XOR Byte Key:", xor_key))	# Just to show the XOR Keys to each byte, for studying the algorithm
+        decrypted_strings.append(enc_data ^ (xor_key & 0xFF))
+    return format_string(decrypted_strings)
+
+pe = pefile.PE("") # <= Put here the latrodectus sample full path
+
+# In all Latrodectus sample, the first byte of the .data section, is the Initial XOR key.
+# Below we collect this first byte
+data_section = next((s for s in pe.sections if b'.data' in s.Name), None)
+data = data_section.get_data()
+first_data_byte = data[0]
+references = []
+for section in pe.sections:
+    if b'.data' in section.Name:
+        data = section.get_data()
+        index = data.find(first_data_byte)
+        while index != -1:
+            references.append(section.VirtualAddress + index)
+            index = data.find(first_data_byte, index + 1)
+
+for ref in references:
+    encryption_key = pe.get_data(ref, 1)[0]
+    print("\033[33m\nXOR Initial Key: \033[0m", hex(encryption_key))
+    data_length = encryption_key ^ pe.get_data(ref + 4, 1)[0]		# Calculate the lenght of the data
+    print("\033[34mEncrypted Data Block Lenght: \033[0m", data_length)
+    encrypted_data = pe.get_data(ref, data_length + 6)[6:]		# Jump six bytes to the initial encrypted block
+    print("\033[31mEncrypted Data Block: \033[0m", hex(int.from_bytes(encrypted_data)))
+    decrypted_str = decrypt_string(encrypted_data, encryption_key)	# Decrypt the strings
+    print("\033[32mDecrypted String:\033[0m", decrypted_str)
+
+pe.close()
